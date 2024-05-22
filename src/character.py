16ad407c9707e145bac264fea2a21bcc140d9482
tapsaca <98756983+tapsaca@users.noreply.github.com>
2024-05-22 14:27:01 +0300

    Use ability modifier instead of score

--- src/character.py
+++ src/character.py
@@ -10,17 +10,23 @@ class Character:
     self.proficiencies = proficiencies
   
   def roll_attack(self, weapon: Weapon):
-    result = random.randint(1, 20) + self.str
+    result = random.randint(1, 20) + self.__ability_modifier(self.str)
     if weapon.name in self.proficiencies or weapon.category in self.proficiencies:
       result += self.__proficiency_bonus()
     return result
 
   def roll_damage(self, weapon: Weapon):
-    dice_rolls = weapon.damage.split("d")
-    damage = self.str
-    for i in range(int(dice_rolls[0])):
-      damage += random.randint(1, int(dice_rolls[1]))
+    rolls_and_die = weapon.damage.split("d")
+    damage = self.__ability_modifier(self.str)
+    for _ in range(int(rolls_and_die[0])):
+      damage += random.randint(1, int(rolls_and_die[1]))
     return damage
+  
+  def __ability_modifier(self, ability_score: int):
+    modifier = ability_score - 10
+    if modifier < 0:
+      return math.ceil(modifier / 2)
+    return math.floor(modifier / 2)
   
   def __proficiency_bonus(self):
     return 1 + (math.ceil(self.level / 4))
